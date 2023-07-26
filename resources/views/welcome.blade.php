0d95a2dc35a2921b781fa8e0cc3d5aa595167650
samir.husen <samir.husen@worldlink.com.np>
2023-07-26 17:46:58 +0545

    route location changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -845,7 +845,7 @@
         @if (Route::has('login'))
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                 @auth
-                    <a href="{{ url('/dashboard') }}"
+                    <a href="{{ url('/dashboard/question/') }}"
                         class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">DASHBOARD</a>
                 @else
                     <a href="{{ route('login') }}"
