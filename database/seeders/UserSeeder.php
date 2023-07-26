41d3db4d95b488ad5a42f613350e81e0c43671b2
samir.husen <samir.husen@worldlink.com.np>
2023-07-26 17:47:29 +0545

    count of random user create updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,6 +27,6 @@ class UserSeeder extends Seeder
             'role' => 'admin'
         ]);
 
-        User::factory()->count(15)->create();
+        User::factory()->count(10)->create();
     }
 }
