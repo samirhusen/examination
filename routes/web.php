b634a46dd44cdea8dec77a5e9ef512a146f1f888
samir.husen <samir.husen@worldlink.com.np>
2023-07-27 11:37:06 +0545

    routes added for students response purpose

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuestionnaireController;
+use App\Http\Controllers\QuestionnaireResponseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +20,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/dashboard/question', QuestionnaireController::class);
 });
 
-require __DIR__.'/auth.php';
+Route::get('/questionnaire/{questionnaireId}/student/{studentId}/{uniqueIdentifier}', [QuestionnaireResponseController::class, 'show'])->name('questionnaire.response.show');
+Route::post('/questionnaire/{questionnaireId}/student/{studentId}/{uniqueIdentifier}', [QuestionnaireResponseController::class, 'submit'])->name('questionnaire.response.submit');
+
+require __DIR__ . '/auth.php';
