04e1fb6f6321c380e3a62ff1977a06e425d16981
samir.husen <samir.husen@worldlink.com.np>
2023-07-27 11:35:00 +0545

    Email build

---
+++ app/Mail/QuestionnaireInvitation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class QuestionnaireInvitation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $url;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function build()
+    {
+        return $this->markdown('emails.questionnaire.invitation')
+            ->with(['url' => $this->url]);
+    }
+}
